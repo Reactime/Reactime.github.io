a9880d9867de026f2c6e588f599b18066641668c
Ruthba Anam <ruthbaanam@gmail.com>
2019-09-30 14:47:55 -0400

    added colors for coding syntax

---
+++ assets/images/test.js
@@ -0,0 +1,10 @@
+npm i reactime
+
+const reactime = require('reactime');
+
+const rootContainer = document.getElementById('root');
+ReactDOM.render(<App />, rootContainer);
+
+reactime(rootContainer);
+
+  reactime(rootcontainer, app)
