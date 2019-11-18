28c4cf80e6c1d8c6559c684a64f5dc9143a30448
prasanna malla <malla.prasanna@gmail.com>
2019-11-18 14:31:36 -0500

    Updates

---
+++ component---src-pages-page-2-js-4ca17805cf6b7f034f91.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Wbzz"),c=n("JZnp"),r=n("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-4ca17805cf6b7f034f91.js.map
