05a7d55187a2c9068469a45370c73d5f98ede26f
Caitlin Chan <caitlinchan23@gmail.com>
2022-06-15 15:38:05 -0700

    Updates

---
+++ component---src-pages-404-js-3d4f9ee72fe0ddf45032.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),u=a(262);t.default=function(){return n.createElement("div",null,n.createElement(u.Z,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3d4f9ee72fe0ddf45032.js.map
